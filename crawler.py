fa2ac29ccef121b5139e597682a929bbe9ff3442
Steph Beneschan <36548653+steph-beneschan-256@users.noreply.github.com>
2022-01-23 17:21:22 -0800

    Update extract_next_links

--- crawler.py
+++ crawler.py
@@ -2,6 +2,10 @@ import logging
 import re
 from urllib.parse import urlparse
 
+from lxml import etree as etree #Would it be better to use BeautifulSoup?
+from lxml import html
+from lxml.html import fromstring
+
 logger = logging.getLogger(__name__)
 
 class Crawler:
@@ -37,9 +41,53 @@ class Crawler:
         absolute form). Validation of links is done later via is_valid method. It is not required to remove duplicates
         that have already been fetched. The frontier takes care of that.
 
+
         Suggested library: lxml
         """
         outputLinks = []
+
+        '''
+        The value of url_data["content_type"] is usually "b'text/html; charset=UTF-8'"
+        This includes the "b'" within the string for some reason (something to do with converting from bytes to str?)
+        Sometimes the encoding isn't present
+        '''
+        if(url_data["content_type"] == None):
+            return outputLinks
+
+        content_type = url_data["content_type"].removeprefix("b\'") #https://docs.python.org/3.9/library/stdtypes.html?highlight=removeprefix#str.removeprefix
+        file_type = content_type.split(';')[0] #usually content_type has both a filetype and an encoding, but sometimes the encoding is absent...
+        
+        #Probably not the best way to check the file type of these checks, but I don't know what else to do...
+        #Also, I think that we might be supposed to handle .txt files too, but I'm not sure if lxml has any tools to parse those
+        doc = None
+        if (file_type == 'text/html'):
+            doc = html.fromstring(url_data["content"]) #Creates an lxml.html.HtmlElement object
+        elif (file_type == 'text/xml'):
+            try:
+                doc = etree.fromstring(url_data["content"], base_url=url_data["url"]) #I'm not entirely sure whether the base_url part helps or not
+            except etree.XMLSyntaxError: #this error seems to come up every time we try to parse an XML file
+                print("failed to parse XML document") #Apparently the lxml parser doesn't work if the XML file has the "XML Prolog" at the beginning
+        
+        if(doc == None):
+            #print("Could not parse {} with content type {}\n".format(url_data["url"], content_type))
+            return outputLinks
+
+        doc.make_links_absolute(url_data["url"])
+
+        #Maybe this could be where we analyze the document for the analytics?
+        #But where do we keep that data? In the frontier? But he said that we don't have to edit frontier.py, right?
+        #If we just stored the data here, then wouldn't it be lost if we had to pause the crawl and then run the program again?
+
+        for link in doc.iterlinks():
+            link_url = link[2] #According to the lxml documentation, link_url should be a tuple: (element, attribute, link, pos)
+            if((link != None) and self.is_valid(link_url)):
+                outputLinks.append(link_url)
+
+        if(file_type == 'text/xml'):
+            print("Successfully parsed an XML file!\n")
+        else:
+            print("Successfully parsed the file")
+
         return outputLinks
 
     def is_valid(self, url):
@@ -48,6 +96,11 @@ class Crawler:
         filter out crawler traps. Duplicated urls will be taken care of by frontier. You don't need to check for duplication
         in this method
         """
+        parsed = urlparse(url)
+
+        #How do we detect calendar traps???
+
+        #original code from skeleton below:
         parsed = urlparse(url)
         if parsed.scheme not in set(["http", "https"]):
             return False
@@ -63,3 +116,4 @@ class Crawler:
             print("TypeError for ", parsed)
             return False
 
+        
