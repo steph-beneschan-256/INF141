2ae8d695ec55177e15bbf724c424b5b2e90d84b0
Steph Beneschan <36548653+steph-beneschan-256@users.noreply.github.com>
2022-01-26 11:23:06 -0800

    Update crawler.py

--- crawler.py
+++ crawler.py
@@ -10,9 +10,11 @@ from lxml import html
 from lxml.html import fromstring
 from lxml.html import soupparser
 import pickle
+from fingerprinter import get_fingerprints
 
 #Import a couple of custom classes
 from analytics_data import Analytics_Data
+from frontier import Frontier
 from string_tokenizer import tokenize
 
 
@@ -24,12 +26,13 @@ class Crawler:
     the frontier
     """
 
+    URL_SIZE_LIMIT = 200 #Is this too high?
     #Directory/file name for analytics file
     #It's probably fastest to use a single file for the analytics data, right?
     #(This is not the file where the final analytics data will be written to once the crawl is finished)
     ANALYTICS_DIR_NAME = "analytics"
     ANALYTICS_FILE_NAME = os.path.join(".", ANALYTICS_DIR_NAME, "analytics_data.pkl")
-    FETCH_LIMIT = 100 #Will only crawl this many URLs; should be useful for testing the analytics functionality
+    FETCH_LIMIT = 100000 #Will only crawl this many URLs; should be useful for testing the analytics functionality
 
     def __init__(self, frontier, corpus):
         self.frontier = frontier
@@ -67,7 +70,7 @@ class Crawler:
     (For testing, call it whenever the program exits)
     '''
     def log_analytics_data(self):
-        self.analytics_data.log_analytics()
+        self.analytics_data.log_analytics(self.frontier.fetched, self.frontier.traps)
 
     def start_crawling(self):
         """
@@ -75,12 +78,13 @@ class Crawler:
         the scraped links to the frontier
         """
 
+        #Note: need to reset analytics data when starting a new crawl
         self.load_analytics_data()
 
-        while self.frontier.has_next_url() and (self.frontier.fetched < self.FETCH_LIMIT):
+        while self.frontier.has_next_url() and ((self.FETCH_LIMIT <= 0) or (self.frontier.fetched < self.FETCH_LIMIT)):
         #while self.frontier.has_next_url():
             url = self.frontier.get_next_url()
-            logger.info("Fetching URL %s ... Fetched: %s, Queue size: %s", url, self.frontier.fetched, len(self.frontier))
+            #logger.info("Fetching URL %s ... Fetched: %s, Queue size: %s", url, self.frontier.fetched, len(self.frontier))
             url_data = self.corpus.fetch_url(url)
 
             for next_link in self.extract_next_links(url_data):
@@ -111,11 +115,16 @@ class Crawler:
         if(url_data["content_type"] == None):
             return outputLinks
 
-        #print(url_data["url"])
-
         content_type = url_data["content_type"].removeprefix("b\'") #https://docs.python.org/3.9/library/stdtypes.html?highlight=removeprefix#str.removeprefix
         file_type = content_type.split(';')[0] #usually content_type has both a filetype and an encoding, but sometimes the encoding is absent...
-        
+
+        '''
+        Misc. notes on file parsing:
+        - We can assume that we only need to decode using UTF-8 
+            https://piazza.com/class/kxzmqq02jne6go?cid=78
+        - 
+        '''
+
         #Probably not the best way to check the file type of these checks, but I don't know what else to do...
         #Also, I think that we might be supposed to handle .txt files too, but I'm not sure if lxml has any tools to parse those
         doc = None
@@ -123,79 +132,64 @@ class Crawler:
             try:
                 doc = html.fromstring(url_data["content"]) #Creates an lxml.html.HtmlElement object
             except:
-                #doc = soupparser.fromstring(url_data["content"]) #apparently soup is slower than lxml?
-                pass #I don't know what to do anymore...
+                #apparently soup is slower than lxml?
+                pass #TODO: handle HTML parsing errors somehow
         elif (file_type == 'text/xml'):
             #print(url_data["content"])
             try:
                 doc = etree.fromstring(url_data["content"])
             except:
-                pass #is there another way???
+                #Getting lxml.etree.XMLSyntaxError
+                pass #TODO: handle XML parsing errors
             
         if(doc == None):
-            #print("Could not parse {} with content type {}\n".format(url_data["url"], content_type))
+            #Could not parse the document
             return outputLinks
 
-        doc.make_links_absolute(url_data["url"])
+        url = (url_data["final_url"] if (url_data["final_url"] != None) else url_data["url"])
 
+        doc.make_links_absolute(url)
+
+        text_no_markup = doc.text_content() #returns object of type lxml.etree._ElementUnicodeResult
+        #text_no_markup = str(html.tostring(doc)) #Uncomment this line to include HTML markup is in the analytics, for testing
+
+        '''
+        Check if this page is a near-duplicate of a previously-examined page.
+        If so, DO NOT assume that it is a trap,
+        but don't return any of its outlinks or count it in the analytics
+        '''
+        fingerprints = get_fingerprints(text_no_markup)
+        print("\tdetecting for duplication for {}".format(url))
+        #The is_near_duplicate method should handle detecting and registering traps
+        #But maybe it should be split into multiple functions?
+        if(self.frontier.is_near_duplicate(url, fingerprints)):
+            return []
+
+        '''
+        Determine the frequency of each word in the page, as well
+        as the overall word count.
+        Then, update the analytics data accordingly.
+        '''
+        page_token_dict = tokenize(text_no_markup)
+        word_count = 0
+        for token in page_token_dict.keys():
+            token_instances = page_token_dict[token]
+            word_count += token_instances
+            self.analytics_data.update_word_frequency(token, token_instances)
+        self.analytics_data.update_longest_page(url, word_count) #If this page doesn't break the record, then nothing will change
+        
+        '''
+        Find any and all valid outlinks within the page.
+        Also, update the analytics data if this page breaks
+        the record for most valid outlinks
+        '''
         valid_links = 0 #For the analytics
         for link in doc.iterlinks():
             link_url = link[2] #According to the lxml documentation, link_url should be a tuple: (element, attribute, link, pos)
             if((link != None) and self.is_valid(link_url)):
                 outputLinks.append(link_url)
                 valid_links += 1
-        self.analytics_data.update_most_valid_outlinks(url_data["url"], valid_links)
-
-        # type: lxml.etree._ElementUnicodeResult
-        text_no_markup = doc.text_content()
-        page_token_dict = tokenize(text_no_markup)
-        
-        word_count = 0
-        for token in page_token_dict.keys():
-            token_instances=  page_token_dict[token]
-            word_count += token_instances
-            self.analytics_data.update_word_frequency(token, token_instances)
-        self.analytics_data.update_longest_page(url_data["url"], word_count)
-        
-
-
-
-
-
-        '''
-        tokenize the fucking shit
-
-        get word/freq dictionary (?)
-
-        word_count = 0
-
-        for each word in that dictionary
-            get the frequency
-            and then
-            add it to the analytics difjapodsfijasdpofajsdf freq. dict. thing for that word
-
-
-
-        '''
-
-
-
-
-        #Maybe this could be where we analyze the document for the analytics?
-        #But where do we keep that data? In the frontier? But he said that we don't have to edit frontier.py, right?
-        #If we just stored the data here, then wouldn't it be lost if we had to pause the crawl and then run the program again?
-        
-
-        #Maybe we could import json and like, store shit in a json dict or some fucking shit?
-        #or use cbor???
-
-
-        try:
-            self.parsed_successfully += 1
-        except:
-            self.parsed_successfully = 1
-
-        #logger.info("{} out of {} documents parsed successfully".format(self.parsed_successfully, self.frontier.fetched))
+        self.analytics_data.update_most_valid_outlinks(url, valid_links) #If this page doesn't break the record, then nothing will change
 
         return outputLinks
 
@@ -205,13 +199,23 @@ class Crawler:
         filter out crawler traps. Duplicated urls will be taken care of by frontier. You don't need to check for duplication
         in this method
         """
-        parsed = urlparse(url)
+        if(len(url) > self.URL_SIZE_LIMIT):
+            #print("trap detected in {}; url too long".format(url))
+            return False
+
+        #Using the frontier to store trap data
+        trimmed = self.frontier.trim_url(url)
+        if(trimmed in self.frontier.traps):
+            print("help")
+            return False
+
+        #TODO: Check for repeating subdomains (maybe if a subdomain appears 3+ times in the URL, declare it invalid?)
+
+        
         #How do we detect calendar traps???
 
-        #If I understand correctly, a different fragment id doesn't denote an entirely different webpage
-        defrag_url = urldefrag(url)[0]
-        if(self.frontier.is_duplicate(defrag_url)):
-            return False
+
+        #https://docs.python.org/3/library/urllib.parse.html#urllib.parse.urlparse
 
         #original code from skeleton below:
         parsed = urlparse(url)
