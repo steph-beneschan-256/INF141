ba8fb7e8cc4faaf896143bf1e48266e0c3954a8c
Steph Beneschan <36548653+steph-beneschan-256@users.noreply.github.com>
2022-01-30 16:37:51 -0800

    Update crawler.py
    * Add helper function for parsing documents
    * Rewrote some of the comments to be more clear for the TA/grader

--- crawler.py
+++ crawler.py
@@ -9,12 +9,12 @@ from lxml import html
 from bs4 import BeautifulSoup
 from lxml.html import soupparser
 import pickle
-from fingerprinter import get_fingerprints
 from collections import defaultdict
 
 #Import a couple of custom classes
 from analytics_data import Analytics_Data
 from string_tokenizer import tokenize
+from fingerprinter import get_fingerprints
 
 # Configures logging and outputting to file
 logging.basicConfig(filename="./history.log", filemode='w', format='%(asctime)s (%(name)s) %(levelname)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p',
@@ -28,14 +28,14 @@ class Crawler:
     the frontier
     """
 
-    URL_SIZE_LIMIT = 300 #Is this too high?
+    URL_SIZE_LIMIT = 300
     DOMAIN_ACCESS_LIMIT = 200
-    #Directory/file name for analytics file
+    #Directory/file name for temporary analytics file
     #It's probably fastest to use a single file for the analytics data, right?
     #(This is not the file where the final analytics data will be written to once the crawl is finished)
     ANALYTICS_DIR_NAME = "analytics"
     ANALYTICS_FILE_NAME = os.path.join(".", ANALYTICS_DIR_NAME, "analytics_data.pkl")
-    FETCH_LIMIT = 0 #Will only crawl this many URLs; should be useful for testing the analytics functionality
+    FETCH_LIMIT = 20 #Will only crawl this many URLs, but ignored if set to zero; can be used for testing
 
     def __init__(self, frontier, corpus):
         self.frontier = frontier
@@ -75,12 +75,11 @@ class Crawler:
         the scraped links to the frontier
         """
 
-        #Note: need to reset analytics data when starting a new crawl
-        #TODO: Add reset function to the analytics data object, and call that here
+        #Load analytics data
         self.load_analytics_data()
 
-        while self.frontier.has_next_url() and ((self.FETCH_LIMIT <= 0) or (self.frontier.fetched < self.FETCH_LIMIT)):
-        #while self.frontier.has_next_url():
+        #while self.frontier.has_next_url() and ((self.FETCH_LIMIT <= 0) or (self.frontier.fetched < self.FETCH_LIMIT)):
+        while self.frontier.has_next_url():
             url = self.frontier.get_next_url()
 
             #added code to check validity before fetching
@@ -96,8 +95,31 @@ class Crawler:
                     if self.corpus.get_file_name(next_link) is not None:
                         self.frontier.add_url(next_link)
 
-        print("Crawling complete.")
+        print("Crawling complete.\nWriting analytics file...")
         self.analytics_data.log_analytics(self.frontier.fetched, self.frontier.get_traps())
+
+    """
+    Parse a document's bytes from url_data["content], then return
+    the parsed lxml object
+
+    If it cannot be parsed, return None
+    """
+    def parse_document(self, content):
+        try:
+            doc = html.fromstring(content)
+            return doc
+        except:
+            pass
+        try:
+            doc = etree.fromstring(content)
+            return doc
+        except:
+            pass
+        try:
+            doc = soupparser.fromstring(content)
+            return doc
+        except:
+            return None
 
     def extract_next_links(self, url_data):
         """
@@ -115,61 +137,70 @@ class Crawler:
         if(url_data["content_type"] == None):
             return []
 
-        '''
-        Determine the whether the page is an HTML or XML document.
-        Due to a presumed error in converting from bytes to str, content_type is usually prefixed by "b'" so we need to remove this.
-
-        Sometimes the encoding isn't present
-        '''
+        #Determine the whether the page is an HTML or XML document.
+        #Due to a presumed error in converting from bytes to str, content_type is usually prefixed by "b'" so we need to remove this.
         content_type = url_data["content_type"].removeprefix("b\'") #https://docs.python.org/3.9/library/stdtypes.html?highlight=removeprefix#str.removeprefix
         file_type = content_type.split(';')[0] #usually content_type has both a filetype and an encoding, but sometimes the encoding is absent...
-        #logger.info(url_data["content_type"])
 
-        '''
-        For some URLs from the fano subdomain, url_data["content"] is a str object rather than a bytes object.
-        In this case, we must convert the object to bytes.
-        Professor said on Piazza that we can assume all documents are encoded in UTF-8
-        '''
+        #For some URLs from the fano subdomain, url_data["content"] is a str object rather than a bytes object.
+        #In this case, we must convert the object to bytes.
+        #(Note: the professor said on Piazza that we can assume all documents are encoded in UTF-8)
         if(not isinstance(url_data["content"], bytes)):
             url_data["content"] = bytes(url_data["content"], 'UTF-8')
 
-        '''
-        Try to parse the document content using lxml.
-        If that does not work, try BeautifulSoup instead.
-        '''
+        #Try to parse the document content using lxml.
+        #If that does not work, try BeautifulSoup instead.
         doc = None
-        try:
-            #Try the HTML and XML parsers
-            if(file_type == 'text/html'):
-                doc = html.fromstring(url_data["content"])
-            elif(file_type == 'text/xml'):
-                doc = etree.fromstring(url_data["content"])
-        except:
-            try:
-                #If the other parsers failed, then try the Beautiful Soup parser.
-                doc = soupparser.fromstring(url_data["content"]) 
-                logger.info("go soup go")
-            except:
-                try:
-                    #Check if the bytes contain excessive null terminators.
-                    #If so, eliminate them.
-                    if(url_data["content"].count(b'\x00') > 0):
-                        logger.info("Excess null terminators found. Eliminating now.")
-                        content_cleaned = url_data["content"].replace(b'\x00', b'')
-                        doc = html.fromstring(content_cleaned)
-                except:
-                    logger.info("Failed to parse the document.")
-                    return []
-            
-        if(doc == None):
-            #Could not parse the document
-            return outputLinks
 
+        doc = self.parse_document(url_data["content"])
+        if(doc == None):
+            try:
+                #Check if the bytes contain excessive null terminators.
+                #If so, eliminate them.
+                if(url_data["content"].count(b'\x00') > 0):
+                    logger.info("Excess null terminators found. Eliminating now.")
+                    content_cleaned = url_data["content"].replace(b'\x00', b'')
+                    doc = self.parse_document(content_cleaned)
+            except:
+                logger.info("Failed to parse the document.")
+                return []
+        if(doc == None):
+            logger.info("Failed to parse the document.")
+            return []
+
+        # try:
+        #     #Try the HTML and XML parsers
+        #     if(file_type == 'text/html'):
+        #         doc = html.fromstring(url_data["content"])
+        #     elif(file_type == 'text/xml'):
+        #         doc = etree.fromstring(url_data["content"])
+        #     else:
+        #         doc = soupparser.fromstring(url_data["content"])
+        # except:
+        #     try:
+        #         #If the other parsers failed, then try the Beautiful Soup parser.
+        #         doc = soupparser.fromstring(url_data["content"]) 
+        #     except:
+        #         try:
+        #             #Check if the bytes contain excessive null terminators.
+        #             #If so, eliminate them.
+        #             if(url_data["content"].count(b'\x00') > 0):
+        #                 logger.info("Excess null terminators found. Eliminating now.")
+        #                 content_cleaned = url_data["content"].replace(b'\x00', b'')
+        #                 doc = html.fromstring(content_cleaned)
+        #         except:
+        #             logger.info("Failed to parse the document.")
+        #             return []
+            
+        # if(doc == None):
+        #     logger.info("Failed to parse the document.")
+        #     return outputLinks
+
+        #Use the final URL, if applicable
         url = (url_data["final_url"] if (url_data["final_url"] != None) else url_data["url"])
 
+        #Update analytics data
         self.analytics_data.new_url_downloaded(url)
-
-        #For analytics
         subdomains = self.extract_subdomains(url)
         for subdomain in subdomains:
             self.analytics_data.update_subdomain_url_count(subdomain)
@@ -179,45 +210,35 @@ class Crawler:
         text_no_markup = doc.text_content() #returns object of type lxml.etree._ElementUnicodeResult
         #text_no_markup = str(html.tostring(doc)) #Uncomment this line to include HTML markup is in the analytics, for testing
 
-        '''
-        Check if this page is a near-duplicate of a previously-examined page.
-        If so, DO NOT assume that it is a trap,
-        but don't return any of its outlinks or count it in the analytics
-        '''
+        # Check if this page is a near-duplicate of a previously-examined page.
+        # If so, DO NOT assume that it is a trap,
+        # but don't return any of its outlinks or count it in the analytics
         fingerprints = get_fingerprints(text_no_markup)
         logger.info("\tchecking duplication for {}".format(url))
-        #The is_near_duplicate method should handle detecting and registering traps
-        #But maybe it should be split into multiple functions?
         if(self.frontier.is_near_duplicate(url, fingerprints)):
             return []
 
-        '''
-        Determine the frequency of each word in the page, as well
-        as the overall word count.
-        Then, update the analytics data accordingly.
-        '''
+        #Determine the frequency of each word in the page, as well as the overall word count.
+        #Update analytics data accordingly.
         page_token_dict = tokenize(text_no_markup)
         word_count = 0
         for token in page_token_dict.keys():
             token_instances = page_token_dict[token]
             word_count += token_instances
             self.analytics_data.update_word_frequency(token, token_instances)
-        self.analytics_data.update_longest_page(url, word_count) #If this page doesn't break the record, then nothing will change
+        #Check if this page breaks the record for highest word count
+        self.analytics_data.update_longest_page(url, word_count)
         
-        '''
-        Find any and all valid outlinks within the page.
-        Also, update the analytics data if this page breaks
-        the record for most valid outlinks
-        '''
+        #Find any and all valid outlinks within the page.
         valid_links = 0 #For the analytics
-        for link in doc.iterlinks():
-            link_url = link[2] #According to the lxml documentation, link_url should be a tuple: (element, attribute, link, pos)
-            #Is it possible to detect duplication of each new link here?
+        for link in doc.iterlinks(): #Link is a tuple of form (element, attribute, link, pos)
+            link_url = link[2] 
             if(link != None):
                 self.counter_links_crawled += 1
                 if(self.is_valid(link_url)):
                     outputLinks.append(link_url)
                     valid_links += 1
+        #Check if this page breaks the record for most valid outlinks
         self.analytics_data.update_most_valid_outlinks(url, valid_links) #If this page doesn't break the record, then nothing will change
 
         return outputLinks
@@ -231,7 +252,7 @@ class Crawler:
         domain = netloc.replace('www.','')
         domain_split = domain.split('.')
         subdomains = []
-        for i in range(len(domain_split) - 2):                          # exlude the 2 root domains: uci.edu and edu
+        for i in range(len(domain_split) - 2): # exlude the 2 root domains: uci.edu and edu
             subdomains.append('.'.join(domain_split[i:]))
         return subdomains
 
@@ -258,14 +279,12 @@ class Crawler:
                 self.frontier.traps.add(url)
                 return False
         
-        #original code from skeleton below:
         parsed = urlparse(url)
         domain = parsed.netloc + parsed.path
 
-        self.counter_domain[domain] += 1
-
         # to avoid calendar trap, track the access amounts
         # the arbitrary and intuitive number I put here is DOMAIN_ACCESS_LIMIT
+        self.counter_domain[domain] += 1
         if self.counter_domain[domain] >= self.DOMAIN_ACCESS_LIMIT:
             self.frontier.traps.add(domain)
         
